8afaa7599da894c8a2e197bd25699941f30a7f2c
dennistobar <dennis.tobar@gmail.com>
2021-04-16 15:57:01 -0400

    feat: using composer to setup project
    Trying to use composer and adding some folder to be excluded from git
    Due composer usage, we must define psr-0 as app and delete lib folder.

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
-$f3=require('lib/base.php');
+
+require_once "vendor/autoload.php";
+
+$f3 = \Base::instance();
 $f3->config('config/config.ini');
 $f3->config('config/routes.ini', true);
 
 $f3->session = new Session();
-$f3->set('ONERROR',function($f3)
-{
-	$logger = new Log('logs/'.date("Ymd").'error.log');
+$f3->set('ONERROR', function ($f3) {
+	$logger = new Log('logs/' . date("Ymd") . 'error.log');
 	echo \Template::instance()->render('error.htm');
 	$e = $f3->get('EXCEPTION');
-	if(!$e instanceof Throwable) {// There is no exception when calling Base->error()
-		$logger->write($f3->get('ERROR.code').": ".$f3->get('ERROR.text')." trace: ". $f3->get('ERROR.trace'), 'r');
+	if (!$e instanceof Throwable) { // There is no exception when calling Base->error()
+		$logger->write($f3->get('ERROR.code') . ": " . $f3->get('ERROR.text') . " trace: " . $f3->get('ERROR.trace'), 'r');
 	}
 });
 
-$f3->logger = new Log($f3->LOG_DIR.date("Ymd").'.log');
+$f3->logger = new Log($f3->LOG_DIR . date("Ymd") . '.log');
 $f3->run();
